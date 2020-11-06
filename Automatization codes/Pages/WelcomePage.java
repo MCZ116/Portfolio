db12ea3d36a76c22a765a626bf83d899f6c65a68
Mateusz Czerwiński <68826557+MCZ116@users.noreply.github.com>
2020-11-06 10:16:42 +0100

    Update WelcomePage.java

--- Automatization codes/Pages/WelcomePage.java
+++ Automatization codes/Pages/WelcomePage.java
@@ -15,7 +15,7 @@ public class WelcomePage{
     @FindBy(className="account")
     WebElement loggedUser;
 
-    @FindBy(xpath="/html/body/div/div[1]/header/div[2]/div/div/nav/div[2]/a")
+    @FindBy(css=".logout")
     WebElement logoutButton;
 
 
